cf3df8cb190c4f14aa71d68bcdb6b7f1989b5b89
Tanbir Mehedi <tanbir.mehedi@gmail.com>
2025-10-03 22:18:50 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,16 @@
+num1 = input("Enter the first number : ")
+
+num2 = input("Enter the second number : ")
+
+num1 = int(num1)
+num2 = int(num2)
+
+a = num1 + num2
+
+s = num1 - num2
+
+m = num1 * num2
+
+d = num1 / num2
+
+print(f"Addition : {a}\nSubtraction : {s}\nMultiplication : {m}\nDivision : {d}")
