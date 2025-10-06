258ebdd01207487d913d0e00132b0c7e4f8d186c
Tanbir Mehedi <tanbir.mehedi@gmail.com>
2025-10-06 22:36:16 +0530

    Add files via upload

---
+++ module_3_task_1.py
@@ -0,0 +1,8 @@
+# Task 1: Check if a Number is Even or Odd
+
+num = int(input("Enter a Number : "))
+
+if num % 2 == 0:
+    print(f"{num} is an Even Number")
+else:
+    print(f"{num} is an odd Number")
---
+++ module_3_task_2.py
@@ -0,0 +1,10 @@
+# Task 2: Sum of Integers from 1 to 50 Using a Loop
+
+sum = 0
+
+for number in range(1,51):
+    sum += number
+
+print("The sum of number 1 to 50 is:", sum)
+
+
